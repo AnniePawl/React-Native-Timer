af841770793b9849393ef4c6cadb74b2992a41df
Anna Pawl <annampawl@gmail.com>
2020-07-14 14:30:05 -0400

    imported redux and react redux

--- App.js
+++ App.js
@@ -1,34 +1,57 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Icon from 'react-native-vector-icons/FontAwesome5';
+import { StyleSheet, ScrollView, Text, View, SafeAreaView } from 'react-native';
+// import { 
+//   Oswald_300Light,
+//   Oswald_400Regular,
+//   Oswald_500Medium,
+//   Oswald_700Bold 
+// } from '@expo-google-fonts/oswald'
 
-function TimerScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Start Timer</Text>
-    </View>
-  );
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+// import Icon from 'react-native-vector-icons/FontAwesome5';
+
+// export default () => {
+//   let [fontsLoaded] = useFonts({
+//     Oswald,
+//   });
+
+export default function All() {
+	return (
+		<View style={styles.container}>
+			<SafeAreaView style={styles.safearea}>
+      <View style={styles.headingContainer}>
+				<Text style={styles.heading}>Timers</Text>
+        </View>
+			</SafeAreaView>
+		</View>
+	);
 }
 
-function MyTimersScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>My Timers </Text>
-    </View>
-  );
-}
+// Styles
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+    backgroundColor:'#ffe6ea',
+		alignItems: 'center',
+		justifyContent: 'center'
+  },
+  headingContainer: {
+    height:60,
+    backgroundColor:'#8ae3ba',
+  },
+	safearea: {
+		flex: 1,
+    width: '100%',
+  }, 
+  heading: {
+    marginTop:15,
+    fontWeight:'bold',
+    textTransform:'uppercase',
+    textAlign:'center',
+    fontSize:35,
+    color:'#ffe6ea',
+   
+  }
 
-const Tab = createBottomTabNavigator();
-
-export default function App() {
-  return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Timer" component={TimerScreen} />
-        <Tab.Screen name="My Timers" component={MyTimersScreen} />
-      </Tab.Navigator>
-    </NavigationContainer>
-  );
-}
+});
